27f5a5039d139ffeb14c8175dc7e485641d9cf00
cdaly <chris@cdaly.me>
2026-02-26 15:50:55 -0500

    feat: export all experiment framework components

--- logos_experiment/__init__.py
+++ logos_experiment/__init__.py
@@ -1,3 +1,11 @@
 from logos_experiment.agent import AgentDefinition, StatefulAgent
+from logos_experiment.config import ExperimentConfig, PipelineStep
+from logos_experiment.runner import ExperimentRunner
 
-__all__ = ["AgentDefinition", "StatefulAgent"]
+__all__ = [
+    "AgentDefinition",
+    "StatefulAgent",
+    "ExperimentConfig",
+    "PipelineStep",
+    "ExperimentRunner",
+]
